97a9a968e87a4ca9f276827d2a7ea1d2ac3c9a61
Muhammad Umair <muhammad.umair@dtechsystems.co>
2023-03-14 23:32:00 +0500

    added qr code

--- src/app/libs/libs-routing.module.ts
+++ src/app/libs/libs-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {InternationalizationComponent} from "./internationalization/internationalization.component";
+import { InternationalizationComponent } from './internationalization/internationalization.component';
+import { QrCodeComponent } from './qr-code/qr-code.component';
 
 const routes: Routes = [
   {
     path: 'i18a',
     component: InternationalizationComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'qr-code',
+    component: QrCodeComponent,
     pathMatch: 'full',
   },
 ];
--- src/app/libs/libs.module.ts
+++ src/app/libs/libs.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TranslateModule} from "@ngx-translate/core";
-import {InternationalizationComponent} from "./internationalization/internationalization.component";
-import {LibsRoutingModule} from "./libs-routing.module";
-
-
+import { TranslateModule } from '@ngx-translate/core';
+import { InternationalizationComponent } from './internationalization/internationalization.component';
+import { LibsRoutingModule } from './libs-routing.module';
+import { QrCodeComponent } from './qr-code/qr-code.component';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
-  declarations: [
-    InternationalizationComponent
-  ],
-  imports: [
-    CommonModule, TranslateModule, LibsRoutingModule
-  ]
+  declarations: [InternationalizationComponent, QrCodeComponent],
+  imports: [CommonModule, TranslateModule, LibsRoutingModule, QRCodeModule],
 })
-export class LibsModule { }
+export class LibsModule {}
