8fccf46f11a60a6876263abc30b131dceb42f96f
Abdul Mannan Saeed <genesistechnologies@protonmail.com>
2023-03-27 03:17:05 +0500

    Header and login fixes

--- client/app/src/auth/auth.module.ts
+++ client/app/src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { SimpleLoginComponent } from './login/templates/simple-login/simple-login.component';
 import { DefaultLoginComponent } from './login/templates/default-login/default-login.component';
 import {TranslateModule} from "@ngx-translate/core";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { PasswordreqestedComponent } from './passwordreqested/passwordreqested.component';
 import { PasswordResetResponseComponent } from './password-reset-response/password-reset-response.component';
@@ -24,6 +24,7 @@ import { PasswordResetResponseComponent } from './password-reset-response/passwo
         CommonModule,
         TranslateModule,
         FormsModule,
+        ReactiveFormsModule,
     ]
 })
 export class AuthModule { }
