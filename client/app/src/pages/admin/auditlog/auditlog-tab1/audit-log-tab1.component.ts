cbaa0f1e02868a3e93eef58f5be626bcc70534dd
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-10-14 18:51:15 +0500

    issue

--- client/app/src/pages/admin/auditlog/auditlog-tab1/audit-log-tab1.component.ts
+++ client/app/src/pages/admin/auditlog/auditlog-tab1/audit-log-tab1.component.ts
@@ -14,9 +14,9 @@ export class AuditLogTab1Component implements OnInit {
   pageSize = 20;
   auditLog: any = new auditlogResolverModel();
 
-             asdafdsfdd=1
+             asdafdsdaasdsfdd=1
 
-  constructor(private auditLogResolver: AuditLogResolver, protected nodeResolver: NodeResolver, protected utilsService: UtilsService) {
+    constructor(private auditLogResolver: AuditLogResolver, protected nodeResolver: NodeResolver, protected utilsService: UtilsService) {
   }
 
   ngOnInit() {
