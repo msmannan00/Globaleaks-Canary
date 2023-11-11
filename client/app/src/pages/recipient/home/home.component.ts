dcc7e2711cae207e737d6652538711d178c2fdf7
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-11-11 19:09:30 +0500

    rounded corner changes

--- client/app/src/pages/recipient/home/home.component.ts
+++ client/app/src/pages/recipient/home/home.component.ts
@@ -1,8 +1,12 @@
 import {Component} from "@angular/core";
+import {AppDataService} from "@app/app-data.service";
 
 @Component({
   selector: "src-recipient-home",
   templateUrl: "./home.component.html"
 })
 export class HomeComponent {
+  constructor(private appDataService:AppDataService) {
+    this.appDataService.updateShowLoadingPanel(false);
+  }
 }
