61da121c093e72df1f610e57f26eef6764954717
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-11-14 12:03:22 +0500

    build improvements

--- client/app/assets/loader.js
+++ client/app/assets/loader.js
@@ -13,50 +13,9 @@ function createAndAppendScript(src) {
   document.getElementsByTagName("body")[0].appendChild(script);
 }
 
-function isBrowserCompatible() {
-  const crawlers = [
-    "Googlebot",
-    "Bingbot",
-    "Slurp",
-    "DuckDuckBot",
-    "Baiduspider",
-    "YandexBot",
-    "Sogou",
-    "Exabot",
-    "ia_archiver"
-  ];
 
-  for (const crawler of crawlers) {
-    if (navigator.userAgent.indexOf(crawler) !== -1) {
-      return true;
-    }
-  }
-
-  if (typeof window === "undefined") {
-    return false;
-  }
-
-  if (window.isSecureContext && !window.crypto && !window.crypto.subtle) {
-    return false;
-  }
-
-  if (!(window.File && window.FileList && window.FileReader)) {
-    return false;
-  }
-
-  return !(typeof Blob === "undefined" ||
-    (!Blob.prototype.slice && !Blob.prototype.webkitSlice && !!Blob.prototype.mozSlice));
-
-
-}
-
-if (!isBrowserCompatible()) {
-  document.getElementById("browser-not-supported").style.display = "block";
-} else {
-  createAndAppendLink("css/styles.css");
-
-  const scriptFiles = ["js/scripts.js", "js/polyfills.js", "js/runtime.js", "js/main.js"];
-  for (const scriptFile of scriptFiles) {
-    createAndAppendScript(scriptFile);
-  }
+createAndAppendLink("css/styles.css");
+const scriptFiles = ["js/scripts.js", "js/polyfills.js", "js/runtime.js", "js/main.js"];
+for (const scriptFile of scriptFiles) {
+  createAndAppendScript(scriptFile);
 }
