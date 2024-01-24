212cf8ef2828e0ad39ed91b0d262172724e106b9
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-01-25 01:54:13 +0500

    channel fixes

--- client/app/src/shared/resolvers/node.resolver.ts
+++ client/app/src/shared/resolvers/node.resolver.ts
@@ -1,10 +1,11 @@
-import {Injectable} from "@angular/core";
-import {PreferenceResolver} from "@app/shared/resolvers/preference.resolver";
-import {Observable, of} from "rxjs";
-import {HttpService} from "@app/shared/services/http.service";
-import {nodeResolverModel} from "@app/models/resolvers/node-resolver-model";
-import {AuthenticationService} from "@app/services/helper/authentication.service";
-import {map} from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { PreferenceResolver } from "@app/shared/resolvers/preference.resolver";
+import { Observable, of, throwError } from "rxjs";
+import { HttpService } from "@app/shared/services/http.service";
+import { nodeResolverModel } from "@app/models/resolvers/node-resolver-model";
+import { AuthenticationService } from "@app/services/helper/authentication.service";
+import { map, catchError } from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: "root"
@@ -12,16 +13,29 @@ import {map} from "rxjs/operators";
 export class NodeResolver {
   dataModel: nodeResolverModel = new nodeResolverModel();
 
-  constructor(private httpService: HttpService, private authenticationService: AuthenticationService, private preferenceResolver: PreferenceResolver) {
-  }
+  constructor(
+      private router: Router,
+      private httpService: HttpService,
+      private authenticationService: AuthenticationService,
+      private preferenceResolver: PreferenceResolver
+  ) {}
 
   resolve(): Observable<boolean> {
-    if (this.authenticationService.session.role === "admin" || (this.authenticationService.session.role === "receiver" && this.preferenceResolver.dataModel.can_edit_general_settings)) {
+    if (
+        this.authenticationService.session.role === "admin" ||
+        (this.authenticationService.session.role === "receiver" &&
+            this.preferenceResolver.dataModel.can_edit_general_settings)
+    ) {
       return this.httpService.requestNodeResource().pipe(
-        map((response: nodeResolverModel) => {
-          this.dataModel = response;
-          return true;
-        })
+          map((response: nodeResolverModel) => {
+            this.dataModel = response;
+            return true;
+          }),
+          catchError((error: any) => {
+            this.authenticationService.deleteSession();
+            this.router.navigateByUrl('/login').then();
+            return throwError(() => error);
+          })
       );
     }
     return of(true);
