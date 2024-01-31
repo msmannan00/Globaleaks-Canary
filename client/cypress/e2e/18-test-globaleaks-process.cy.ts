eccfeeb5c43d58ef0e73cb452a3a0a2bbb1fc6ec
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-01-31 20:57:00 +0500

    process fixes

--- client/cypress/e2e/18-test-globaleaks-process.cy.ts
+++ client/cypress/e2e/18-test-globaleaks-process.cy.ts
@@ -34,16 +34,13 @@ describe("globaleaks process", function () {
         cy.get("#tip-action-star").click();
       });
 
-      cy.get(".tip-action-download-file").should("have.length", 2);
-      cy.get(".tip-action-download-file").eq(0).click();
+      cy.get('#fileListBody', { timeout: 10000 }).find('tr').should('have.length', 2);
 
       const comment = "comment";
-
       cy.get("[name='newCommentContent']").type(comment);
       cy.get("#comment-action-send").click();
-
-      cy.get("#comment-0").should("contain", comment);
-
+      cy.waitForLoader();
+      cy.get('#comment-0').should('contain', comment);
       cy.visit("/#/recipient/reports");
       cy.takeScreenshot("recipient/reports", 0);
 
@@ -76,6 +73,8 @@ describe("globaleaks process", function () {
           const changeEvent = new Event("change", { bubbles: true });
           input[0].dispatchEvent(changeEvent);
         });
+        /* file upload delay */
+        cy.wait(3000)
 
         cy.get("#files-action-confirm").click();
         cy.get(".progress-bar").should("exist", { timeout: 10000, retryOnStatusCodeFailure: true });
@@ -93,8 +92,9 @@ describe("globaleaks process", function () {
       cy.get(".TipInfoID").first().invoke("text").then(t => {
         expect(t.trim()).to.be.a("string");
       });
+      cy.waitForLoader();
       cy.get('[id="tip-action-silence"]').should('be.visible', { timeout: 10000 }).click();
-      cy.get('#tip-action-notify').should('be.visible', { timeout: 10000 }).should('be.visible', { timeout: 10000 }).click();
+      cy.get('#tip-action-notify').should('be.visible', { timeout: 10000 }).click();
       cy.get('#tip-action-silence').should('be.visible', { timeout: 10000 }).should('be.visible');
       cy.takeScreenshot("recipient/report", 0);
 
