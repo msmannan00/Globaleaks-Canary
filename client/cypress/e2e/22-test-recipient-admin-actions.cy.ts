185dd668b5edb02db7bd24c6757fbb64e76356d7
Abdul Mannan Saeed <msmannan00@protonmail.com>
2024-03-01 11:55:43 +0500

    bumped to latest client

--- client/cypress/e2e/22-test-recipient-admin-actions.cy.ts
+++ client/cypress/e2e/22-test-recipient-admin-actions.cy.ts
@@ -1,4 +1,4 @@
-describe("Action: Revoke Access and Grant Access - Recipient Section", () => {
+describe("TESTS", () => {
   it("should revoke and grant access to reports", function () {
     cy.login_receiver();
 
@@ -29,14 +29,13 @@ describe("Action: Revoke Access and Grant Access - Recipient Section", () => {
     cy.get('#assignSubmissionStatus', { timeout: 10000 }).select(1);
     cy.get('textarea[name="reason"]').type("This is a close status illatest motivation");
     cy.get("#modal-action-ok").click();
-
     cy.get("#tip-action-reopen-status").click();
     cy.get('textarea[name="motivation"]').type("This is a Reopen status test motivation");
     cy.get("#modal-action-ok").click();
 
     cy.logout();
   });
-  it("should open a new tab for reports", function () {
+  it("recipient should file a report on behalf of whistleblower", function () {
     cy.login_receiver();
 
     cy.visit("/#/recipient/reports");
@@ -46,7 +45,7 @@ describe("Action: Revoke Access and Grant Access - Recipient Section", () => {
     cy.logout();
   });
 
-  it("should set a reminder for reports", function () {
+  it("should set a reminder date for reports", function () {
     cy.login_receiver();
 
     cy.visit("/#/recipient/reports");
@@ -79,45 +78,34 @@ describe("Action: Revoke Access and Grant Access - Recipient Section", () => {
     cy.get("#add-submission-sub-status").click();
 
     cy.logout();
-
   });
-  it("should change sub-status for reports", function () {
-    cy.login_receiver();
-
-    cy.visit("/#/recipient/reports");
-    cy.get("#tip-0").first().click();
-    cy.get("#tip-action-change-status").click();
-    cy.wait(5000);
-    cy.get('#assignSubmissionStatus', { timeout: 10000 }).select(0);
-    cy.get('textarea[name="reason"]').type("This is a test motivation");
-    cy.get("#modal-action-ok").click();
-
-    cy.logout();
-  });
-  it("should import questionnaire file and add report", () => {
+  it("should update default context", () => {
     cy.login_admin();
-
-    cy.visit("/#/admin/questionnaires");
-    cy.get("#keyUpload").click();
-    cy.fixture("files/testing.txt").then(fileContent => {
-      cy.get('input[type="file"]').then(input => {
-        const blob = new Blob([fileContent], { type: "text/plain" });
-        const testFile = new File([blob], "files/testing.txt");
-        const dataTransfer = new DataTransfer();
-        dataTransfer.items.add(testFile);
-        const inputElement = input[0] as HTMLInputElement;
-        inputElement.files = dataTransfer.files;
-
-        const changeEvent = new Event("change", { bubbles: true });
-        input[0].dispatchEvent(changeEvent);
-      });
-
-    });
     cy.visit("/#/admin/contexts");
     cy.get("#edit_context").first().click();
     cy.get('select[name="contextResolver.questionnaire_id"]').select('Testing');
     cy.get("#save_context").click();
-
+    cy.logout();
+  })
+  it("should change sub-status for reports", function () {
+    cy.login_receiver();
+    cy.visit("/#/recipient/reports");
+    cy.get("#tip-0").first().click();
+    cy.get("#tip-action-change-status").click();
+    cy.get('#assignSubmissionStatus', { timeout: 10000 }).select(0);
+    cy.get('textarea[name="reason"]').type("This is a test motivation");
+    cy.get("#modal-action-ok").click();
+    cy.logout();
+  });
+  it("should update default context", () => {
+    cy.login_admin();
+    cy.visit("/#/admin/contexts");
+    cy.get("#edit_context").first().click();
+    cy.get('select[name="contextResolver.questionnaire_id"]').select('Testing');
+    cy.get("#save_context").click();
+    cy.logout();
+  })
+  it("should run audio questionnaire", () => {
     cy.visit("#/");
     cy.get("#WhistleblowingButton").click();
     cy.get("#step-0").should("be.visible");
@@ -126,16 +114,21 @@ describe("Action: Revoke Access and Grant Access - Recipient Section", () => {
     cy.wait(6000);
     cy.get("#stop_recording").click();
     cy.get("#NextStepButton").click();
+    cy.get("#SubmitButton").should("be.visible");
+    cy.get("#SubmitButton").click();
+  })
+  it("should run identity questionnaire", () => {
+    cy.visit("#/");
+    cy.get("#WhistleblowingButton").click();
+    cy.get("#NextStepButton").click();
     cy.get("input[type='text']").eq(1).should("be.visible").type("abc");
     cy.get("input[type='text']").eq(2).should("be.visible").type("xyz");
     cy.get("select").first().select(1);
-    cy.wait(1000);
     cy.get("#SubmitButton").should("be.visible");
-
     cy.get("#SubmitButton").click();
-    
-    cy.get('.mt-md-3.clearfix.ng-star-inserted').find('#ReceiptButton').click(); 
-
+    cy.get('.mt-md-3.clearfix.ng-star-inserted').find('#ReceiptButton').click();
     cy.logout();
   });
+
+
 });
