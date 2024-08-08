b67837e4e0e659add742df9ebdd40999a61ed94d
Faizan Javed <101192444+faizanjaved38@users.noreply.github.com>
2024-08-08 19:03:40 +0500

    code refactor in migration

--- backend/globaleaks/db/migrations/update_69/__init__.py
+++ backend/globaleaks/db/migrations/update_69/__init__.py
@@ -7,6 +7,7 @@ from globaleaks.utils.utility import datetime_now
 from globaleaks.models.config_desc import ConfigDescriptor
 from globaleaks.models.config import get_default
 from globaleaks.db.appdata import load_appdata
+from sqlalchemy import tuple_
 
 class Tenant_v_68(Model):
     __tablename__ = 'tenant'
@@ -17,72 +18,94 @@ class Tenant_v_68(Model):
     active = Column(Boolean, default=False, nullable=False)
 
 class MigrationScript(MigrationBase):
+    default_tenant_keys = ["subdomain", "onionservice", "https_admin", "https_analyst", "https_cert" ,"wizard_done", "uuid", "mode", "default_language", "name"]
 
     def migrate_Tenant(self):
-        # Define default tenant keys that should not be removed
-        default_tenant_keys = ["subdomain", "onionservice", "https_admin", "https_analyst", "https_cert",
-                               "wizard_done", "uuid", "mode", "default_language", "name"]
+
+        old_tenants = self.session_old.query(self.model_from['Tenant']).all()
+        new_tenants = []
+        for old_obj in old_tenants:
+            new_tenant = self.model_to['Tenant']()
+            for key in new_tenant.__mapper__.column_attrs.keys():
+                setattr(new_tenant, key, getattr(old_obj, key, None))
+            new_tenants.append(new_tenant)
     
-        # Migrate existing tenants
-        tenant_query = self.session_old.query(self.model_from['Tenant'])
-        total_tenants = tenant_query.count()
+        defualt_tenant = self.model_to['Tenant']()
+        defualt_tenant.id = 1000000
+        defualt_tenant.active = False
+        new_tenants.append(defualt_tenant)
+
+        self.session_new.add_all(new_tenants)
+        self.entries_count['Tenant'] = len(new_tenants)
     
-        for old_obj in tenant_query:
-            new_obj = self.model_to['Tenant']()
+    def migrate_Config(self):
+
+        old_configs = self.session_old.query(self.model_from['Config']).all()
+        new_configs = []
+        for old_obj in old_configs:
+            new_obj = self.model_to['Config']()
             for key in new_obj.__mapper__.column_attrs.keys():
-                setattr(new_obj, key, getattr(old_obj, key, None))
-            self.session_new.add(new_obj)
+                setattr(new_obj, key, getattr(old_obj, key))
+            new_configs.append(new_obj)
+        self.session_new.bulk_save_objects(new_configs)
     
-        # Add a new tenant with id 1000000
-        new_tenant = self.model_to['Tenant']()
-        new_tenant.id = 1000000
-        new_tenant.creation_date = datetime_now()
-        new_tenant.active = False
-        self.session_new.add(new_tenant)
-        self.entries_count['Tenant'] += 1
-        self.session_new.commit()
-    
-        # Handle configurations
-        self.handle_configurations(total_tenants, default_tenant_keys)
-    
-    def handle_configurations(self, total_tenants, default_tenant_keys):
-        # Insert default configurations for the new tenant
         variables = {name: get_default(desc.default) for name, desc in ConfigDescriptor.items()}
+        new_default_configs = []
         for var_name, value in variables.items():
-            if not self.session_new.query(self.model_to['Config']).filter_by(tid=1000000, var_name=var_name).first():
-                new_config = self.model_to['Config']()
-                new_config.tid = 1000000
-                new_config.var_name = var_name
-                new_config.value = value
-                new_config.update_date = datetime_now()
-                self.session_new.add(new_config)
-                self.entries_count['Config'] += 1
+            new_config = self.model_to['Config']()
+            new_config.tid = 1000000
+            new_config.var_name = var_name
+            new_config.value = value
+            new_default_configs.append(new_config)
+        self.session_new.bulk_save_objects(new_default_configs)
+        self.entries_count['Config'] += len(new_default_configs)
+    
+        special_entries = [('tenant_counter', self.session_old.query(self.model_from['Tenant']).count()),('profile_counter', 999999)]
+        special_configs = []
+        for var_name, value in special_entries:
+            new_config = self.model_to['Config']()
+            new_config.tid = 1
+            new_config.var_name = var_name
+            new_config.value = value
+            special_configs.append(new_config)
+        self.session_new.bulk_save_objects(special_configs)
+        self.entries_count['Config'] += len(special_configs)
+    
+        default_config = {entry.var_name: entry.value for entry in self.session_new.query(self.model_to['Config']).filter_by(tid=1000000).all()}
+        tenant_configs = self.session_new.query(self.model_to['Config'].tid,self.model_to['Config'].var_name,self.model_to['Config'].value).filter(self.model_to['Config'].tid.notin_([1000000, 1])).all()
+    
+        to_delete = []
+        for tid, var_name, value in tenant_configs:
+            if var_name in default_config and value == default_config[var_name] and var_name not in self.default_tenant_keys:
+                to_delete.append((tid, var_name))
+    
+        if to_delete:
+            self.session_new.query(self.model_to['Config']).filter(tuple_(self.model_to['Config'].tid, self.model_to['Config'].var_name).in_(to_delete)).delete(synchronize_session=False)
+            self.entries_count['Config'] -= len(to_delete)
+
+    def migrate_ConfigL10N(self):
+
+        old_configs = self.session_old.query(self.model_from['ConfigL10N']).all()
+        new_configs = []
+        for old_obj in old_configs:
+            new_obj = self.model_to['ConfigL10N']()
+            for key in new_obj.__mapper__.column_attrs.keys():
+                setattr(new_obj, key, getattr(old_obj, key))
+            new_configs.append(new_obj)
+        self.session_new.bulk_save_objects(new_configs)
+    
         models.config.add_new_lang(self.session_new, 1000000, 'en', load_appdata())
         self.entries_count['ConfigL10N'] += 72
         self.entries_count['EnabledLanguage'] += 1
-        # Set up special configuration entries for tid=1
-        special_entries = [('tenant_counter', total_tenants),('profile_counter', 999999)]
-        for var_name, value in special_entries:
-            if not self.session_new.query(self.model_to['Config']).filter_by(tid=1, var_name=var_name).first():
-                new_config = self.model_to['Config']()
-                new_config.tid = 1
-                new_config.var_name = var_name
-                new_config.value = value
-                new_config.update_date = datetime_now()
-                self.session_new.add(new_config)
-                self.entries_count['Config'] += 1
-        self.session_new.commit()
     
-        # Remove redundant configurations for other tenants
-        default_config = {entry.var_name: entry.value for entry in self.session_new.query(self.model_to['Config']).filter_by(tid=1000000).all()}
-        tenant_ids = [tid for tid, in self.session_new.query(self.model_to['Config'].tid).distinct().filter(self.model_to['Config'].tid.notin_([1000000, 1])).all()]
+        default_config = {(entry.var_name, entry.lang): entry.value for entry in self.session_new.query(self.model_to['ConfigL10N']).filter_by(tid=1000000).all()}
+        tenant_configs = self.session_new.query(self.model_to['ConfigL10N'].tid,self.model_to['ConfigL10N'].var_name,self.model_to['ConfigL10N'].lang,self.model_to['ConfigL10N'].value).filter(self.model_to['ConfigL10N'].tid.notin_([1000000, 1])).all()
     
-        for tid in tenant_ids:
-            tenant_config = {entry.var_name: entry.value for entry in self.session_new.query(self.model_to['Config']).filter_by(tid=tid).all()}
-            for var_name, value in tenant_config.items():
-                if var_name in default_config and value == default_config[var_name] and var_name not in default_tenant_keys:
-                    entry = self.session_new.query(self.model_to['Config']).filter_by(tid=tid, var_name=var_name).one_or_none()
-                    if entry:
-                        self.session_new.delete(entry)
-                        self.entries_count['Config'] -= 1
-                        self.session_new.commit()
+        to_delete = []
+        for tid, var_name, lang, value in tenant_configs:
+            if (var_name, lang) in default_config and value == default_config[(var_name, lang)] and var_name not in self.default_tenant_keys:
+                to_delete.append((tid, var_name, lang))
+    
+        if to_delete:
+            self.session_new.query(self.model_to['ConfigL10N']).filter(tuple_(self.model_to['ConfigL10N'].tid, self.model_to['ConfigL10N'].var_name, self.model_to['ConfigL10N'].lang).in_(to_delete)).delete(synchronize_session=False)
+            self.entries_count['ConfigL10N'] -= len(to_delete)
