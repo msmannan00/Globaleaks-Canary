9acde50706ad74446970f9044f33a5957b0c7a3e
Faizan Javed <101192444+faizanjaved38@users.noreply.github.com>
2024-09-05 18:48:55 +0500

    default variables added

--- backend/globaleaks/models/config.py
+++ backend/globaleaks/models/config.py
@@ -14,7 +14,7 @@ import copy
 # List of variables that on creation are set with the value
 # they have on the root tenant
 inherit_from_root_tenant = ['default_questionnaire']
-default_tenant_keys = ["subdomain", "onionservice", "https_admin", "https_analyst", "https_cert", "wizard_done", "uuid", "mode", "default_language", "name", "disable_submissions", "encryption", "https_whistleblower"]
+default_tenant_keys = ["subdomain", "onionservice", "https_admin", "https_analyst", "https_cert", "wizard_done", "uuid", "mode", "default_language", "name", "disable_submissions", "encryption", "https_whistleblower", "receipt_salt", "counter_submissions", "crypto_escrow_pub_key"]
 
 
 def get_default(default):
