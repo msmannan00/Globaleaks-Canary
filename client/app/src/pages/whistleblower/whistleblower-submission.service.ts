3324f24366383975c360373b7b76053d5573c908
Faizan Javed <101192444+faizanjaved38@users.noreply.github.com>
2024-09-19 18:47:32 +0500

    Update version to 5.0.8

--- client/app/src/pages/whistleblower/whistleblower-submission.service.ts
+++ client/app/src/pages/whistleblower/whistleblower-submission.service.ts
@@ -4,46 +4,53 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WhistleblowerSubmissionService {
-
   constructor() { }
 
   checkForInvalidFields(scope:any) {
+    let enabled_counter = 0;
     for (let counter = 0; counter <= scope.navigation; counter++) {
       scope.validate[counter] = true;
       if (scope.questionnaire.steps[counter].enabled) {
-        if (scope.stepForms.get(counter)?.invalid) {
+        if (scope.stepForms.get(enabled_counter)?.invalid) {
           scope.navigation = counter;
           return false;
         }
+	enabled_counter++;
       }
     }
     return true;
   }
-  decrementStep(scope:any) {
-    if (scope.hasPreviousStep()) {
-      for (let i = scope.navigation - 1; i >= scope.firstStepIndex(); i--) {
-        if (i === -1 || scope.fieldUtilitiesService.isFieldTriggered(null, scope.questionnaire.steps[i], scope.answers, scope.score)) {
-          scope.navigation = i;
-          scope.utilsService.scrollToTop();
-          return;
-        }
-      }
-    }
-  };
 
-  incrementStep(scope:any) {
-    if (!scope.runValidation()) {
+  decrementStep(scope:any) {
+    if (!scope.hasPreviousStep()) {
       return;
     }
-    if (scope.hasNextStep()) {
-      for (let i = scope.navigation + 1; i <= scope.lastStepIndex(); i++) {
-        if (scope.fieldUtilitiesService.isFieldTriggered(null, scope.questionnaire.steps[i], scope.answers, scope.score)) {
-          scope.navigation = i;
-          scope.utilsService.scrollToTop();
-          return;
-        }
+
+    for (let i = scope.navigation - 1; i >= scope.firstStepIndex(); i--) {
+      if (i === -1 || scope.fieldUtilitiesService.isFieldTriggered(null, scope.questionnaire.steps[i], scope.answers, scope.score)) {
+        scope.navigation = i;
+        scope.utilsService.scrollToTop();
+        return;
       }
     }
   }
 
+  incrementStep(scope:any) {
+    if (!scope.hasNextStep()) {
+      return;
+    }
+
+    if (!scope.runValidation()) {
+      scope.utilsService.scrollToTop();
+      return;
+    }
+
+    for (let i = scope.navigation + 1; i <= scope.lastStepIndex(); i++) {
+      if (scope.fieldUtilitiesService.isFieldTriggered(null, scope.questionnaire.steps[i], scope.answers, scope.score)) {
+        scope.navigation = i;
+        scope.utilsService.scrollToTop();
+        return;
+      }
+    }
+  }
 }
--- client/app/src/shared/services/utils.service.ts
+++ client/app/src/shared/services/utils.service.ts
@@ -273,6 +273,7 @@ export class UtilsService {
       this.modalService.open(RequestSupportComponent,{backdrop: "static",keyboard: false});
     }
   }
+
   array_to_map(receivers: any) {
     const ret: any = {};
 
@@ -291,13 +292,12 @@ export class UtilsService {
     let text;
     for (let i = 0; i < submission_statuses.length; i++) {
       if (submission_statuses[i].id === status) {
-        text = submission_statuses[i].label;
-
+        text = this.translateService.instant(submission_statuses[i].label);
 
         const subStatus = submission_statuses[i].substatuses;
         for (let j = 0; j < subStatus.length; j++) {
-          if (subStatus[j].id === substatus) {
-            text += ' \u2013 ' + this.translateService.instant(subStatus[j].label);
+          if (subStatus[j].id === substatus && subStatus[j].label) {
+            text += ' \u2013 ' + subStatus[j].label;
             break;
           }
         }
@@ -305,6 +305,23 @@ export class UtilsService {
       }
     }
     return text?text:"";
+  }
+
+  searchInObject(obj: any, searchTerm: string) {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
+
+        if (typeof value === 'string' && value.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return true;
+        } else if (typeof value === 'object') {
+          if (this.searchInObject(value, searchTerm)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
   }
 
   isDatePassed(time: string) {
@@ -408,19 +425,6 @@ export class UtilsService {
     window.print();
   }
 
-  saveAs(authenticationService: AuthenticationService, filename: any, url: string): void {
-
-    const headers = new HttpHeaders({
-      "X-Session": authenticationService.session.id
-    });
-
-    this.http.get(url, {responseType: "blob", headers: headers}).subscribe(
-      response => {
-        this.saveBlobAs(filename, response);
-      }
-    );
-  }
-
   saveBlobAs(filename:string,response:Blob){
     const blob = new Blob([response], {type: "text/plain;charset=utf-8"});
     const blobUrl = URL.createObjectURL(blob);
@@ -433,6 +437,18 @@ export class UtilsService {
     setTimeout(() => {
       URL.revokeObjectURL(blobUrl);
     }, 1000);
+  }
+
+  saveAs(authenticationService: AuthenticationService, filename: any, url: string): void {
+    const headers = new HttpHeaders({
+      "X-Session": authenticationService.session.id
+    });
+
+    this.http.get(url, {responseType: "blob", headers: headers}).subscribe(
+      response => {
+        this.saveBlobAs(filename, response);
+      }
+    );
   }
 
   getPostponeDate(ttl: number): Date {
